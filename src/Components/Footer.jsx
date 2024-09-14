42d2c5e0346b66ca3360b41bd77d3f9b139f4c10
LuDev01 <luisatoro0121@hotmail.com>
2024-09-13 21:56:16 -0500

    Footer completed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,9 +6,16 @@ const Footer = () => {
     <footer >
       <div className='footer-div'>
         <p className='footer-text'>Powered By: Luisa Toro & Julian Callejas</p>
+        <div className='footer-logo-icons'>
+          <img className='footer-img' src="/images/DH.png" alt='DH-logo' />
+          <div className='footer-all-icons'>
+            <img className='footer-icons' src="/images/ico-facebook.png"></img>
+            <img className='footer-icons' src="/images/ico-facebook.png"></img>
+            <img className='footer-icons' src="/images/ico-facebook.png"></img>
+            <img className='footer-icons' src="/images/ico-facebook.png"></img>
+          </div>
+        </div>
       </div>
-        <img className='footer-img' src="/images/DH.png" alt='DH-logo' />
-    <img src></img>
     </footer>
   )
 }

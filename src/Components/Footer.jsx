17ed68f813f175b217124b5a57e1bb267536efe3
LuDev01 <luisatoro0121@hotmail.com>
2024-09-13 21:21:21 -0500

    Form and footer  (partial modifications)

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
+
 const Footer = () => {
   return (
-    <footer>
-        <p>Powered by</p>
-        <img src="./img/DH.png" alt='DH-logo' />
+    <footer >
+      <div className='footer-div'>
+        <p className='footer-text'>Powered By: Luisa Toro & Julian Callejas</p>
+      </div>
+        <img className='footer-img' src="/images/DH.png" alt='DH-logo' />
+    <img src></img>
     </footer>
   )
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,40 @@
-import React from "react";
-
+import { useState } from 'react';
+import TextField from './TextField'
+import { FormControl } from '@mui/joy';
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+
+const [data, setData] = useState({name:'',email:''})
+
+const handleChange=(e)=>{
+
+  e.preventDefault();
+  setData({...data,[e.target.name]:e.target.value})
+
+}
+
+const [prueba, setPrueba] = useState("")
 
   return (
     <div>
       <form>
+        <FormControl sx={{marginBottom:'20px'}}>
+          <TextField
+            type='text'
+            placeholder='Max Doe'
+            label='name'
+          //  value={data.name}
+          //  handleChange={handleChange}
+          />
+        </FormControl>
+
+        <TextField
+          type='email'
+          placeholder='example@email.com'
+          label='email'
+          value={prueba}
+          handleChange={(e)=>{setPrueba(e.target.value)}}
+        />
       </form>
     </div>
   );
